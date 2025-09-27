36ddc7ab2480d45a451944b23f776d339077cfa6
flaviofagundespro <seu-email@example.com>
2025-09-27 19:10:48 -0300

    FIX: Reorganiza a estrutura do projeto para o padrão com pasta src

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './App.css' // Se o seu App.css for o CSS principal
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
